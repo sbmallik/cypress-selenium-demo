4266bd22799ec97bf08e50133657c5654801c0e4
sbmallik <smallik@gannett.com>
2021-04-13 02:45:00 -0400

    QE-7883 - Replace test to avoid using internal address.

---
+++ cypress/integration/simpleDemo.spec.js
@@ -0,0 +1,22 @@
+/// <reference types="Cypress" />
+  
+describe('A simple demo', () => {
+  before(() => {
+    cy.visit('/');
+  });
+  beforeEach(() => {
+    cy.eyesOpen();
+  });
+  afterEach(() => {
+    cy.eyesClose();
+  });
+  it('Applitools visual grid', () => {
+    cy.get('#log-in').click()
+    cy.wait(3000)
+      .eyesCheckWindow({
+      tag: 'App window',
+      target: 'window',
+      fully: true
+    });
+  });
+});
