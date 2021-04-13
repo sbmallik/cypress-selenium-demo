a7ae5e085ceaf43d2d850dcc580a93eb674ab147
sbmallik <smallik@gannett.com>
2021-04-13 17:30:41 -0400

    QE-7883 - Reinstate the external file.

---
+++ cypress/integration/simpleDemo.spec.js
@@ -0,0 +1,25 @@
+/// <reference types="Cypress" />
+
+describe('A simple demo', () => {
+  before(() => {
+    cy.visit('https://demo.applitools.com');
+  });
+  beforeEach(() => {
+    cy.eyesOpen({
+      appName: 'Demo App',
+      testName: 'Ultrafast grid demo'
+    });
+  });
+  afterEach(() => {
+    cy.eyesClose();
+  });
+  it('Applitools visual grid', () => {
+    cy.get('#log-in').click()
+    cy.wait(3000)
+      .eyesCheckWindow({
+      tag: 'App window',
+      target: 'window',
+      fully: true
+    });
+  });
+});
