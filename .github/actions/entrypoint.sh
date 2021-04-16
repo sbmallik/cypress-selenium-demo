722dd2c95533b8844e5f28760646bcdb59c63f94
Sudip Mallik <smallik@gannett.com>
2021-04-16 10:30:02 -0400

    add-github-param: Add github server as a parameter in the actions file. (#4)

--- .github/actions/entrypoint.sh
+++ .github/actions/entrypoint.sh
@@ -3,6 +3,6 @@
 statusCode=$(curl -w '%{http_code}' -X POST \
   -H 'Content-Type:application/json' \
   -d '{"serverId":"github.com","commitSha":"'$1'"}' \
-  "$3/api/externals/github/servers/github.com/commit/$1/complete?apiKey=$2")
+  "$3/api/externals/github/servers/$4/commit/$1/complete?apiKey=$2")
 
 echo "::set-output name=response::$statusCode"
